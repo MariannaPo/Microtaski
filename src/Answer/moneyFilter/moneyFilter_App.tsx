a1d08d9b9d4d716864418bb5122f35ce2ac56d84
MariannaPo <mapodmasko@gmail.com>
2022-08-18 18:31:08 +0300

    filter

---
+++ src/Answer/moneyFilter/moneyFilter_App.tsx
@@ -0,0 +1,45 @@
+// import React, {useState} from 'react';
+// import './App.css';
+// import NewComponent from "./NewComponent";
+//
+// type FilterType= "all"| 'RUBLS'| "Dollars"
+//
+//
+// function App() {
+//
+//     const [money, setMoney] = useState([
+//         {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+//         {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+//         {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+//         {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+//         {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+//         {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+//         {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+//         {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+//     ])
+//
+//     const [filter, setFilter] = useState<FilterType>('all')
+//
+//     let currentMoney = money;
+//     if (filter === "Dollars") {
+//         currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === "Dollars")
+//     }
+//     if (filter=== "RUBLS") {
+//         currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === "RUBLS")
+//     }
+//
+//
+//     const onClickFilterHandler = (nameButton: FilterType) => {
+//         setFilter(nameButton)
+//     }
+//
+//     return (
+//         <>
+//             <NewComponent currentMoney={currentMoney} onHandler={onClickFilterHandler}/>
+//
+//         </>
+//     );
+// }
+//
+// export default App;
+export default {};
---
+++ src/Answer/moneyFilter/moneyFilter_Component.tsx
@@ -0,0 +1,45 @@
+// import React from "react";
+//
+// type NewComponentProps = {
+//     currentMoney: Array<{
+//         banknots: string;
+//         value: number;
+//         number: string;
+//     }>;
+//     onHandler: (
+//         nameButton: "all" | 'RUBLS' | "Dollars"
+//     ) => void
+// }
+//
+//
+// const NewComponent = (props: NewComponentProps) => {
+//         return (
+//             <>
+//                 <ul>
+//                     {
+//                         props.currentMoney.map((objFromMoneyArr, index) => {
+//                             return (
+//                                 <li key={
+//                                     index
+//                                 }
+//                                 >
+//                                     <span>{objFromMoneyArr.banknots}</span>
+//                                     <span>{objFromMoneyArr.value}</span>
+//                                     <span>{objFromMoneyArr.number}</span>
+//                                 </li>
+//                             )
+//                         })
+//                     }
+//                 </ul>
+//                 <div style={{marginLeft: '35px'}}>
+//                     <button onClick={() => props.onHandler('all')}>all</button>
+//                     <button onClick={() => props.onHandler('RUBLS')}>RUBLES</button>
+//                     <button onClick={() => props.onHandler('Dollars')}>Dollars</button>
+//                 </div>
+//             </>
+//         )
+//             ;
+//     }
+// ;
+// export default NewComponent;
+export default {};
