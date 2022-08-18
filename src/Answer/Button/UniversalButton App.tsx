4c97df03f8c4b37b75582375b740a8b8edda8576
MariannaPo <mapodmasko@gmail.com>
2022-08-18 15:44:42 +0300

    useState

--- src/Answer/Button/UniversalButton App.tsx
+++ src/Answer/Button/UniversalButton App.tsx
@@ -1,31 +1,32 @@
-import React from 'react';
-import './App.css';
-import Button from "./Components/Button";
-
-
-function App() {
-
-    const Button1Foo = (subscriber: string, age: number) => {
-        console.log(subscriber, age)
-    }
-
-    const Button2Foo = (subscriber: string, age: number) => {
-        console.log(subscriber, age)
-    }
-
-    const OtherButton = () => {
-        console.log('Im stupid button')
-    }
-
-    return (
-        <div className="App">
-            {/*<button>MyYouTubeChannel-1</button>*/}
-            {/*<button>MyYouTubeChannel-2</button>*/}
-            <Button name={'MyYouTubeChannel-1'} callBack={() => Button1Foo('Im VASYA', 21)}/>
-            <Button name={'MyYouTubeChannel-2'} callBack={() => Button2Foo('Im IVAN', 34)}/>
-            <Button name={"Channel-3"} callBack={OtherButton}/>
-        </div>
-    );
-}
-
-export default App;
+// import React from 'react';
+// import './App.css';
+// // import Button from "./Components/Button";
+//
+//
+// function App() {
+//
+//     const Button1Foo = (subscriber: string, age: number) => {
+//         console.log(subscriber, age)
+//     }
+//
+//     const Button2Foo = (subscriber: string, age: number) => {
+//         console.log(subscriber, age)
+//     }
+//
+//     const OtherButton = () => {
+//         console.log('Im stupid button')
+//     }
+//
+//     return (
+//         <div className="App">
+//             {/*<button>MyYouTubeChannel-1</button>*/}
+//             {/*<button>MyYouTubeChannel-2</button>*/}
+//             <Button name={'MyYouTubeChannel-1'} callBack={() => Button1Foo('Im VASYA', 21)}/>
+//             <Button name={'MyYouTubeChannel-2'} callBack={() => Button2Foo('Im IVAN', 34)}/>
+//             <Button name={"Channel-3"} callBack={OtherButton}/>
+//         </div>
+//     );
+// }
+//
+// export default App;
+export default {};
---
+++ src/Answer/useState.tsx
@@ -0,0 +1,30 @@
+// import React, {useState} from 'react';
+// import './App.css';
+// import {log} from "util";
+//
+//
+// function App() {
+//
+//     // let a = 1;
+//     let [a, setA] = useState(1)
+//
+//     const onClickHandler = () => {
+//         setA(++a);
+//         console.log(a)
+//     }
+//
+//     const onClickHandlerZero = () => {
+//         setA(0)
+//         return console.log(0);
+//     }
+//     return (
+//         <div className="App">
+//             <h1>{a}</h1>
+//             <button onClick={onClickHandler}>number</button>
+//             <button onClick={onClickHandlerZero}>0</button>
+//         </div>
+//     );
+// }
+//
+// export default App;
+export default {};
