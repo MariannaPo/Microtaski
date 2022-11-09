218a973a7874b3efb613dd9b567093b63068aff1
MariannaPo <mapodmasko@gmail.com>
2022-11-09 18:17:17 +0300

    filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import NewComponent from "./NewComponent";
+
+type FilterType= "all"| 'RUBLS'| "Dollars"
+
 
 function App() {
+
+    const [money, setMoney] = useState([
+        {banknots: 'Dollars', value: 100, number: ' a1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' z1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' w1234567890'},
+        {banknots: 'Dollars', value: 100, number: ' e1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' c1234567890'},
+        {banknots: 'RUBLS', value: 100, number: ' r1234567890'},
+        {banknots: 'Dollars', value: 50, number: ' x1234567890'},
+        {banknots: 'RUBLS', value: 50, number: ' v1234567890'},
+    ])
+
+    const [filter, setFilter] = useState<FilterType>('all')
+
+    let currentMoney = money;
+    if (filter === "Dollars") {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === "Dollars")
+    }
+    if (filter=== "RUBLS") {
+        currentMoney = money.filter((filteredMoney) => filteredMoney.banknots === "RUBLS")
+    }
+
+
+    const onClickFilterHandler = (nameButton: FilterType) => {
+        setFilter(nameButton)
+    }
+
     return (
-        <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
+        <>
+            <NewComponent currentMoney={currentMoney} onHandler={onClickFilterHandler}/>
+        </>
     );
 }
 
